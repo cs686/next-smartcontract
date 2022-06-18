665f4d868446dc642177acc7ceda7dacd84aa2b2
chongsongsong <chongsongsong@lixiang.com>
2022-06-18 13:31:55 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.ignoreWarnings = [
+      {
+        message: /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+      }
+      
+    ]
+    return config
+  },
 }
 
 module.exports = nextConfig
